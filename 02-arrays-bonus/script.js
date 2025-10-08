5d2bf1668f573810deb5a1c80a533fc16796391b
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-08 15:51:57 +0200

    ex 2.5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -47,4 +47,8 @@ console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+for (i=0; i<teachers.length; i++) {
+  teachersString += teachers[i]+", "
+}
+console.log(teachersString);
