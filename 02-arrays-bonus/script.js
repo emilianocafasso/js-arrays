04dfaf595694d7a5e979088f5f0aade732f90114
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-08 18:16:23 +0200

    fix 2.1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -12,7 +12,7 @@ const teachers = [
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = [];
 for (let i=0; i<teachers.length; i++) {
-  reversedTeachers[i] = teachers[teachers.length-i-1]
+  reversedTeachers.push (teachers[teachers.length-i-1])
 }
 console.log(reversedTeachers)
 
@@ -20,7 +20,6 @@ console.log(reversedTeachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
-
 for(i=0; i<teachers.length; i++) {
   if(teachers[i].length >= 5) {
     longNames.push (teachers[i])
@@ -34,7 +33,6 @@ if(index_Ed!= -1) {
   teachers.splice(index_Ed,1)
 }
 console.log(teachers);
-
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
